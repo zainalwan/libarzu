be47dc6d6c8a89ce880f9224be72cff01647b28f
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2021-12-13 14:31:36 +0700

    Change main.cpp
    - Add two cases for testing purpose

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@ int main(int argc, char* argv[]) {
     Zain::Arzu arzu(argc, &argv[0]);
     std::cout << "Argument '--two' is " << arzu.getValue("--two") << '\n';
     std::cout << "Index of argument '--one' is " << arzu.index("--one") << '\n';
+    std::cout << "Index of argument '--six' is " << arzu.index("--six") << '\n';
     std::cout << "Argument '-t' exists: " << arzu.exists("-t") << '\n';
     std::cout << "Argument '-f' exists: " << arzu.exists("-f") << '\n';
+    std::cout << "Argument '--five' is " << arzu.getValue("--five") << '\n';
 
     return 0;
 }
